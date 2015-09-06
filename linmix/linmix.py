2f5e2b5d9cbfc0d25c17a4133929756812c4a2f5
jmeyers314 <jmeyers314@gmail.com>
2015-09-05 23:07:47 -0700

    PEP8

--- linmix/linmix.py
+++ linmix/linmix.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 
+
 class Chain(object):
     def __init__(self, x, y, xsig, ysig, xycov, delta, K, nchains):
         self.x = np.array(x, dtype=float)
@@ -44,24 +45,21 @@ class Chain(object):
 
         self.initialized = False
 
-    def initial_guess(self): # Step 1
+    def initial_guess(self):  # Step 1
         # For convenience
         x = self.x
         y = self.y
-        xsig = self.xsig
-        ysig = self.ysig
         xycov = self.xycov
-        xycorr = self.xycorr
         xvar = self.xvar
         yvar = self.yvar
         N = self.N
         K = self.K
 
         # Use BCES estimator for initial guess of theta = {alpha, beta, sigsqr}
-        self.beta = ((np.cov(x, y, ddof=1)[1,0] - np.mean(xycov))
+        self.beta = ((np.cov(x, y, ddof=1)[1, 0] - np.mean(xycov))
                      / (np.var(x, ddof=1) - np.mean(xvar)))
         self.alpha = np.mean(y) - self.beta * np.mean(x)
-        self.sigsqr = np.var(y, ddof=1) - np.mean(yvar) - self.beta * (np.cov(x, y, ddof=1)[1,0]
+        self.sigsqr = np.var(y, ddof=1) - np.mean(yvar) - self.beta * (np.cov(x, y, ddof=1)[1, 0]
                                                                        - np.mean(xycov))
         self.sigsqr = np.max([self.sigsqr,
                               0.05 * np.var(y - self.alpha - self.beta * x, ddof=1)])
@@ -72,7 +70,7 @@ class Chain(object):
 
         # Now get an MCMC value dispersed around above values
         X = np.ones((N, 2), dtype=float)
-        X[:,1] = x
+        X[:, 1] = x
         Sigma = np.linalg.inv(np.dot(X.T, X)) * self.sigsqr
         coef = np.random.multivariate_normal([0, 0], Sigma)
         chisqr = np.random.chisquare(self.nchains)
@@ -111,7 +109,7 @@ class Chain(object):
             for i in xrange(N):
                 minind = np.argmin(abs(x[i] - self.mu))
                 pig[minind] += 1
-                self.G[i,minind] = 1
+                self.G[i, minind] = 1
             self.pi = np.random.dirichlet(pig+1)
 
         self.eta = y.copy()
@@ -122,7 +120,7 @@ class Chain(object):
 
         self.initialized = True
 
-    def update_cens_y(self): # Step 2
+    def update_cens_y(self):  # Step 2
         todo = self.cens[:]
         while len(todo) > 0:
             self.y[todo] = np.random.normal(loc=self.eta[todo],
@@ -130,12 +128,12 @@ class Chain(object):
                                             size=len(todo))
             todo = np.nonzero(np.logical_not(self.delta) & (self.y > self.y_ul))[0]
 
-    def update_xi(self): # Step 3
+    def update_xi(self):  # Step 3
         wxerr = self.wxerr
         wyerr = self.wyerr
 
         # Eqn (58)
-        sigma_xihat_ik_sqr = 1.0/(1.0/(self.xvar * (1.0 - self.xycorr**2))[:,np.newaxis]
+        sigma_xihat_ik_sqr = 1.0/(1.0/(self.xvar * (1.0 - self.xycorr**2))[:, np.newaxis]
                                   + self.beta**2 / self.sigsqr
                                   + 1.0/self.tausqr)
         # Eqn (57)
@@ -144,18 +142,18 @@ class Chain(object):
         xihat_xy_i = self.x.copy()
         xihat_xy_i[wyerr] += (self.xycov / self.yvar * (self.eta - self.y))[wyerr]
         # Eqn (55)
-        xihat_ik = (sigma_xihat_i_sqr[:,np.newaxis]
-                           * ((xihat_xy_i/self.xvar
-                               * (1.0 - self.xycorr**2))[:,np.newaxis]
-                              + self.beta*(self.eta[:,np.newaxis] - self.alpha)/self.sigsqr
-                              + self.mu/self.tausqr))
+        xihat_ik = (sigma_xihat_i_sqr[:, np.newaxis]
+                    * ((xihat_xy_i/self.xvar
+                        * (1.0 - self.xycorr**2))[:, np.newaxis]
+                       + self.beta*(self.eta[:, np.newaxis] - self.alpha)/self.sigsqr
+                       + self.mu/self.tausqr))
         # Eqn (54)
         xihat_i = np.sum(self.G * xihat_ik, axis=1)
         # Eqn (53)
         self.xi[wxerr] = np.random.normal(loc=xihat_i[wxerr],
                                           scale=np.sqrt(sigma_xihat_i_sqr[wxerr]))
 
-    def update_eta(self): # Step 4
+    def update_eta(self):  # Step 4
         wxerr = self.wxerr
         wyerr = self.wyerr
 
@@ -167,23 +165,23 @@ class Chain(object):
         sigma_etahat_i_sqr = 1.0/(1.0/etaxyvar + 1.0/self.sigsqr)
         # Eqn (67)
         etahat_i = (sigma_etahat_i_sqr * (etaxy / etaxyvar
-                       + (self.alpha + self.beta * self.xi) / self.sigsqr))
+                    + (self.alpha + self.beta * self.xi) / self.sigsqr))
         # Eqn (66)
         self.eta[wyerr] = np.random.normal(loc=etahat_i[wyerr],
                                            scale=np.sqrt(sigma_etahat_i_sqr[wyerr]))
 
-    def update_G(self): # Step 5
+    def update_G(self):  # Step 5
         # Eqn (74)
         piNp = self.pi * (1.0/np.sqrt(2.0*np.pi*self.tausqr)
-                          * np.exp(-0.5 * (self.xi[:,np.newaxis] - self.mu)**2 / self.tausqr))
-        q_ki = piNp / np.sum(piNp, axis=1)[:,np.newaxis]
+                          * np.exp(-0.5 * (self.xi[:, np.newaxis] - self.mu)**2 / self.tausqr))
+        q_ki = piNp / np.sum(piNp, axis=1)[:, np.newaxis]
         # Eqn (73)
         for i in xrange(self.N):
             self.G[i] = np.random.multinomial(1, q_ki[i])
 
-    def update_alpha_beta(self): # Step 6
+    def update_alpha_beta(self):  # Step 6
         X = np.ones((self.N, 2), dtype=float)
-        X[:,1] = self.xi
+        X[:, 1] = self.xi
         # Eqn (77)
         XTXinv = np.linalg.inv(np.dot(X.T, X))
         Sigma_chat = XTXinv * self.sigsqr
@@ -192,7 +190,7 @@ class Chain(object):
         # Eqn (75)
         self.alpha, self.beta = np.random.multivariate_normal(chat, Sigma_chat)
 
-    def update_sigsqr(self): # Step 7
+    def update_sigsqr(self):  # Step 7
         # Eqn (80)
         ssqr = 1.0/(self.N-2) * np.sum((self.eta - self.alpha - self.beta * self.xi)**2)
         # Eqn (79)
@@ -200,14 +198,14 @@ class Chain(object):
         # Eqn (78)
         self.sigsqr = nu * ssqr / np.random.chisquare(nu)
 
-    def update_pi(self): # Step 8
+    def update_pi(self):  # Step 8
         # Eqn (82)
         self.nk = np.sum(self.G, axis=0)
         # Eqn (81)
         self.pi = np.random.dirichlet(self.nk+1)
 
-    def update_mu(self): # Step 9
-        Gsum = np.sum(self.G * self.xi[:,np.newaxis], axis=0)
+    def update_mu(self):  # Step 9
+        Gsum = np.sum(self.G * self.xi[:, np.newaxis], axis=0)
         for k in xrange(self.K):
             if self.nk[k] != 0:
                 # Eqn (86)
@@ -221,21 +219,21 @@ class Chain(object):
             else:
                 self.mu[k] = np.random.normal(loc=self.mu0, scale=np.sqrt(self.usqr))
 
-    def update_tausqr(self): # Step 10
+    def update_tausqr(self):  # Step 10
         # Eqn (88)
         nu_k = self.nk + 1
         # Eqn (89)
-        tk_sqr = 1.0/nu_k * (self.wsqr + np.sum(self.G*(self.xi[:,np.newaxis]-self.mu)**2, axis=0))
+        tk_sqr = 1.0/nu_k * (self.wsqr + np.sum(self.G*(self.xi[:, np.newaxis]-self.mu)**2, axis=0))
         # Eqn (87)
         self.tausqr = tk_sqr * nu_k / np.random.chisquare(nu_k, size=self.K)
 
-    def update_mu0(self): # Step 11
+    def update_mu0(self):  # Step 11
         # Eqn (94)
         mubar = np.mean(self.mu)
         # Eqn (93)
         self.mu0 = np.random.normal(loc=mubar, scale=np.sqrt(self.usqr/self.K))
 
-    def update_usqr(self): # Step 12
+    def update_usqr(self):  # Step 12
         # Eqn (96)
         nu_u = self.K + 1
         # Eqn (97)
@@ -245,7 +243,7 @@ class Chain(object):
             usqr = usqrhat * nu_u / np.random.chisquare(nu_u)
         self.usqr = usqr
 
-    def update_wsqr(self): # Step 13
+    def update_wsqr(self):  # Step 13
         # Eqn (102)
         a = 0.5 * (self.K + 3)
         # Eqn (103)
@@ -311,7 +309,7 @@ class Chain(object):
 
 
 class LinMix(object):
-    """ A class to perform linear regression of `y` on `x` when there are measurement errors in 
+    """ A class to perform linear regression of `y` on `x` when there are measurement errors in
     both variables.  The regression assumes:
 
     eta = alpha + beta * xi + epsilon
@@ -320,13 +318,13 @@ class LinMix(object):
 
     y = eta + yerr
 
-    Here, `alpha` and `beta` are the regression coefficients, `epsilon` is the intrinsic random 
-    scatter about the regression, `xerr` is the measurement error in `x`, and `yerr` is the 
-    measurement error in `y`.  `epsilon` is assumed to be normally-distributed with mean zero and 
-    variance `sigsqr`.  `xerr` and `yerr` are assumed to be normally-distributed with means equal 
-    to zero, variances `xsig`^2 and `ysig`^2, respectively, and covariance `xycov`. The 
-    distribution of `xi` is modelled as a mixture of normals, with group proportions `pi`, means 
-    `mu`, and variances `tausqr`.  
+    Here, `alpha` and `beta` are the regression coefficients, `epsilon` is the intrinsic random
+    scatter about the regression, `xerr` is the measurement error in `x`, and `yerr` is the
+    measurement error in `y`.  `epsilon` is assumed to be normally-distributed with mean zero and
+    variance `sigsqr`.  `xerr` and `yerr` are assumed to be normally-distributed with means equal
+    to zero, variances `xsig`^2 and `ysig`^2, respectively, and covariance `xycov`. The
+    distribution of `xi` is modelled as a mixture of normals, with group proportions `pi`, means
+    `mu`, and variances `tausqr`.
 
     Args:
         x(array_like): The observed independent variable.
@@ -334,19 +332,19 @@ class LinMix(object):
         xsig(array_like): 1-sigma measurement errors in x.
         ysig(array_like): 1-sigma measurement errors in y.
         xycov(array_like): Covariance between the measurement errors in x and y.
-        delta(array_like): Array indicating whether a data point is censored (i.e., not detected), 
-            or not.  If delta[i] == 1, then the ith source is detected.  If delta[i] == 0, then 
+        delta(array_like): Array indicating whether a data point is censored (i.e., not detected),
+            or not.  If delta[i] == 1, then the ith source is detected.  If delta[i] == 0, then
             the ith source is not detected and y[i] will be interpreted as an upper limit.  Note
-            that if there are censored data points, then the maximum-likelihood estiate 
-            (alpha, beta, sigsqr) is not valid.  By default, all data points are assumed to be 
+            that if there are censored data points, then the maximum-likelihood estimate
+            (alpha, beta, sigsqr) is not valid.  By default, all data points are assumed to be
             detected.
         K(int): The number of Gaussians to use in the mixture model for the distribution of xi.
         nchains(int): The number of Monte Carlo Markov Chains to instantiate.
 
     Attributes:
         nchains(int): The number of instantiated MCMCs.
-        chain(numpy recarray): The concatenated MCMCs themselves.  Actually, only the concatenation 
-            of the last half of each chain is stored here after convergence is reached.  The 
+        chain(numpy recarray): The concatenated MCMCs themselves.  Actually, only the concatenation
+            of the last half of each chain is stored here after convergence is reached.  The
             recarray has the following columns:
                 - alpha(float): The regression intercept.
                 - beta(float): The regression slope.
@@ -354,15 +352,15 @@ class LinMix(object):
                 - pi(array_like): The mixture model component fractions.
                 - mu(array_like): The mixture model component means.
                 - tausqr(array_like): The mixture model component variances.
-                - mu0(float): The hyperparameter describing the prior variance of the distribution 
+                - mu0(float): The hyperparameter describing the prior variance of the distribution
                     of mixture means.
-                - usqr(float): The hyperparameter describing the prior variance of the distribution 
+                - usqr(float): The hyperparameter describing the prior variance of the distribution
                     of mixture variances.
-                - wsqr(float): The hyperparameter describing the typical scale for the prior on 
+                - wsqr(float): The hyperparameter describing the typical scale for the prior on
                     `usqr` and `tausqr`.
-                - ximean(float): The mean of the distribution for the independent latent variable 
+                - ximean(float): The mean of the distribution for the independent latent variable
                     `xi`.
-                - xisig(float): The standard deviation of the distribution for the independent 
+                - xisig(float): The standard deviation of the distribution for the independent
                     latent variable `xi`.
                 - corr(float): The linear correlation coefficient between the latent dependent and
                     independent variables `xi` and `eta`.
@@ -376,20 +374,20 @@ class LinMix(object):
         c0 = self.chains[0]
         ndraw = c0.ichain/2
         psi = np.empty((ndraw, self.nchains, 6), dtype=float)
-        psi[:,:,0] = np.vstack([c.chain['alpha'][0:ndraw] for c in self.chains]).T
+        psi[:, :, 0] = np.vstack([c.chain['alpha'][0:ndraw] for c in self.chains]).T
         beta = np.vstack([c.chain['beta'][0:ndraw] for c in self.chains]).T
-        psi[:,:,1] = beta
+        psi[:, :, 1] = beta
         sigsqr = np.vstack([c.chain['sigsqr'][0:ndraw] for c in self.chains]).T
-        psi[:,:,2] = np.log(sigsqr)
+        psi[:, :, 2] = np.log(sigsqr)
         ximean = np.vstack([np.sum(c.chain['pi'][0:ndraw] * c.chain['mu'][0:ndraw], axis=1)
-                                for c in self.chains]).T
-        psi[:,:,3] = ximean
+                            for c in self.chains]).T
+        psi[:, :, 3] = ximean
         xivar = np.vstack([np.sum(c.chain['pi'][0:ndraw] * (c.chain['tausqr'][0:ndraw] +
                                                             c.chain['mu'][0:ndraw]**2),
                                   axis=1)
                            for c in self.chains]).T - ximean**2
-        psi[:,:,4] = xivar
-        psi[:,:,5] = np.arctanh(beta * np.sqrt(xivar / (beta**2 * xivar + sigsqr)))
+        psi[:, :, 4] = xivar
+        psi[:, :, 5] = np.arctanh(beta * np.sqrt(xivar / (beta**2 * xivar + sigsqr)))
         return psi
 
     def _get_Rhat(self):
@@ -397,7 +395,7 @@ class LinMix(object):
         ndraw = psi.shape[0]
         psibarj = np.sum(psi, axis=0)/ndraw
         psibar = np.mean(psibarj, axis=0)
-        sjsqr = np.sum((psi-psibarj)**2 / (ndraw-1.0), axis=(0,1))
+        sjsqr = np.sum((psi-psibarj)**2 / (ndraw-1.0), axis=(0, 1))
         Bvar = ndraw / (self.nchains-1.0) * np.sum((psibarj-psibar)**2, axis=0)
         Wvar = sjsqr / self.nchains
         varplus = (1.0 - 1.0 / ndraw) * Wvar + Bvar / ndraw
@@ -407,15 +405,15 @@ class LinMix(object):
     def run_mcmc(self, miniter=5000, maxiter=100000, silent=False):
         """ Run the Markov Chain Monte Carlo for the LinMix object.
 
-        Bayesian inference is employed, and a Markov chain containing random draws from the 
-        posterior is developed.  Convergence of the MCMC to the posterior is monitored using the 
-        potential scale reduction factor (RHAT, Gelman et al. 2004). In general, when RHAT < 1.1 
-        then approximate convergence is reached.  After convergence is reached, the second halves 
+        Bayesian inference is employed, and a Markov chain containing random draws from the
+        posterior is developed.  Convergence of the MCMC to the posterior is monitored using the
+        potential scale reduction factor (RHAT, Gelman et al. 2004). In general, when RHAT < 1.1
+        then approximate convergence is reached.  After convergence is reached, the second halves
         of all chains are concatenated and stored in the `.chain` attribute as a numpy recarray.
 
         Args:
             miniter(int): The minimum number of iterations to use.
-            maxiter(int): The maximum number of iterations to use. 
+            maxiter(int): The maximum number of iterations to use.
             silent(bool): If true, then suppress updates during sampling.
         """
         checkiter = 100
