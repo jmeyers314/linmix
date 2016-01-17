0d214d237f32fd4ddb03c535778f68b6f52447e9
Josh Meyers <jmeyers314@gmail.com>
2016-01-16 20:03:43 -0800

    clean up line in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ https://github.com/pypa/sampleproject
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 # To use a consistent encoding
-from codecs import open
 from os import path
 
 here = path.abspath(path.dirname(__file__))
